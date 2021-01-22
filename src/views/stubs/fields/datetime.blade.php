6c5941adea406e9cb80a62b1a1cf04e68aab8f29
Seryak <misha-seryak@ya.ru>
2021-01-22 19:55:09 +0700

    почистил от лишнего

--- src/views/stubs/fields/datetime.blade.php
+++ src/views/stubs/fields/datetime.blade.php
@@ -6,16 +6,6 @@
            @if($input->required)required="required"@endif>
 </div>
 
-{{--<div data-field="{{$input->field_name_dotted }}" class="hystmodal" id="CropModal" aria-hidden="true">--}}
-{{--    <div class="hystmodal__wrap">--}}
-{{--        <div class="hystmodal__window" role="dialog" aria-modal="true">--}}
-{{--            <div data-hystclose class="hystmodal__close">Закрыть</div>--}}
-{{--            <img id="image" data-field="{{$input->field_name_dotted }}" src="" alt="Image preview...">--}}
-{{--            <div class="button save">Сохранить</div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
-
 <script>
 $(function() {
     $('input[name="{{$input->field_name }}"]').flatpickr(
