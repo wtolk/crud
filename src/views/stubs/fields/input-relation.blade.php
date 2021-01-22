20772afbe157f82fb5ffe23af19a9d8613b5262e
Seryak <misha-seryak@ya.ru>
2021-01-22 13:02:37 +0700

    selectize для поля отношений

--- src/views/stubs/fields/input-relation.blade.php
+++ src/views/stubs/fields/input-relation.blade.php
@@ -5,7 +5,23 @@
         <option value="{{$input->empty['value']}}" @if($input->field_value == $input->empty['value']) selected @endif > {{$input->empty['name']}} </option>
         @endif
         @foreach($input->options as $value => $option)
-        <option value="{{$value}}" @if($input->field_value === $value) selected @endif > {{$option}} </option>
+        <option value="{{$value}}" @if($input->field_value === $value or $input->default_value === $value) selected @endif > {{$option}} </option>
         @endforeach
     </select>
 </div>
+
+<script>
+$(function() {
+    $('select[name="{{$input->field_name }}"]').selectize({
+        plugins: ['remove_button'],
+        delimiter: ',',
+        persist: false,
+        create: function(input) {
+            return {
+                value: input,
+                text: input
+            }
+        }
+    });
+});
+</script>
