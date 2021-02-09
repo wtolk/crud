d3bc4a8a63e6e42d85f0be5fc8ab63f56800e2cb
Seryak <misha-seryak@ya.ru>
2021-02-09 10:48:17 +0700

    Summernote + Elfinder

--- src/views/stubs/fields/input-summernote.blade.php
+++ src/views/stubs/fields/input-summernote.blade.php
@@ -1,11 +1,3 @@
-{{--<div class="field">--}}
-{{--    <label for="title">{{$input->title}}</label>--}}
-{{--    <input type="text" name="{{$input->field_name }}" class="form-control"--}}
-{{--           value="{{ $input->field_value }}" placeholder="{{$input->placeholder}}"--}}
-{{--           @if($input->required)required="required"@endif>--}}
-{{--</div>--}}
-
-
 <div class="field no-bg">
     <label for="title">{{$input->title}}</label>
     <textarea name="{{$input->field_name }}"
@@ -15,15 +7,26 @@
 
 
 
-
+<!-- jQuery and jQuery UI (REQUIRED) -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
+<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="/vendor/wtolk/crud/elfinder/css/elfinder.min.css">
+<link rel="stylesheet" type="text/css" href="/vendor/wtolk/crud/elfinder/css/theme.css">
 
 <script src="/vendor/wtolk/crud/summernote/summernote-lite.js"></script>
+<script src="/vendor/wtolk/crud/elfinder/js/elfinder.min.js"></script>
+<script src="/vendor/wtolk/crud/elfinder/js/i18n/elfinder.ru.js"></script>
 
 <script src="/vendor/wtolk/crud/summernote/lang/summernote-ru-RU.min.js"></script>
 <script src="/vendor/wtolk/crud/summernote/plugins/summernote-ext-elfinder.js"></script>
 <script src="/vendor/wtolk/crud/summernote/plugins/summernote-file.js"></script>
 <script src="/vendor/wtolk/crud/summernote/plugins/summernote-ext-template.js"></script>
 <link href="/vendor/wtolk/crud/summernote/summernote-lite.min.css" rel="stylesheet">
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.4.33/example1/colorbox.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.colorbox/1.4.33/jquery.colorbox-min.js"></script>
+
 
 <script>
     $(function() {
@@ -36,12 +39,15 @@
             toolbar: [
                 ['style', ['style']],
                 ['font', ['bold', 'underline', 'clear']],
-                ['color', ['color']],
+                // ['color', ['color']],
                 ['para', ['ul', 'ol', 'paragraph']],
                 ['table', ['table']],
-                ['insert', ['link', 'picture', 'video', 'file']],
-                ['view', ['fullscreen', 'codeview', 'help', 'template']]
+                // ['insert', ['link', 'picture', 'video', ['elfinder'], 'file']],
+                ['insert', ['link',  'video', 'elfinder']],
+                // ['view', ['fullscreen', 'codeview', 'help', 'template']]
+                ['view', ['fullscreen', 'codeview',]]
             ],
+
             template: {
                 path: '/vendor/wtolk/crud/summernote/templates', // path to your template folder
 
@@ -60,5 +66,26 @@
         });
     });
 
-
+    // Elfinder Plugin
+    function elfinderDialog(context){ // <------------------ +context
+        var fm = $('<div/>').dialogelfinder({
+            url : "{{ route('adfm.elfinder.route') }}",
+            lang : 'ru',
+            width : 840,
+            height: 450,
+            destroyOnClose : true,
+            getFileCallback : function(file, fm) {
+                // console.log(file);
+                // console.log('инсерт');
+                // $('.editor').summernote('editor.insertImage', fm.convAbsUrl(file.url)); ...before
+                context.invoke('editor.insertImage', fm.convAbsUrl(file.url)); // <------------ after
+            },
+            commandsOptions : {
+                getfile : {
+                    oncomplete : 'close',
+                    folders : false
+                }
+            }
+        }).dialogelfinder('instance');
+    }
 </script>
