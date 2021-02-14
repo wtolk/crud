ac4370ad92295bb0253866a16ecf7fadbd597b9a
Seryak <misha-seryak@ya.ru>
2021-02-14 14:29:27 +0700

    Подтверждение для ссылок

--- src/views/stubs/fields/link.blade.php
+++ src/views/stubs/fields/link.blade.php
@@ -1,12 +1,19 @@
+@php
+    $confirm = false;
+    if ($link->title == 'Удалить' || $link->method == 'DELETE') {
+        $confirm = true;
+    }
+@endphp
+
 @if(!$link->href)
 @php($link->href = route($link->route, $link->route_param))
 @endif
 @if($link->method == 'GET')
-<a @if($link->class)class="button"@endif href="{{$link->href}}">{!! $link->title !!}</a>
+<a @if($link->class)class="button  @if($confirm) confirm-action @endif "@endif href="{{$link->href}}">{!! $link->title !!}</a>
 @else
 <form action="{{$link->href}}" method="POST">
     @method($link->method)
     @csrf
-    <button class="link">{!! $link->title !!}</button>
+    <button class="link @if($confirm) confirm-action @endif">{!! $link->title !!}</button>
 </form>
 @endif
