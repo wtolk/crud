9ed99ffe83f917b802fd35d4e73be8bc613173ac
BlWoAlf <blwoalf@inbox.ru>
2021-06-16 14:35:58 +0700

    add catalog menu items

--- src/FormPresenter.php
+++ src/FormPresenter.php
@@ -51,7 +51,8 @@ class FormPresenter
         $this->mainMenu = [
             ItemMenu::make('Страницы')->route('adfm.pages.index')->icon("insert_drive_file"),
             ItemMenu::make('Меню')->route('adfm.menus.index')->icon("menu"),
-//            ItemMenu::make('Товары')->route('adfm.products.index')->icon('card_giftcard'),
+        //     ItemMenu::make('Категории')->route('adfm.categories.index')->icon('article'),
+        //    ItemMenu::make('Товары')->route('adfm.products.index')->icon('card_giftcard'),
            ItemMenu::make('Обратная сязь')->route('adfm.feedbacks.index')->icon('feedback'),
 
             'Конфигурация' => [
